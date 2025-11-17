f031603ebd16f58d2b3d1f071ae5ddb4ea8fc96d
JanviMangukiya <janvimangukiya1708@gmail.com>
2025-11-17 15:56:17 +0530

    added rate limit

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,9 +1,9 @@
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 5,
-    message: "Too Many Requests, Please Try Again After 1 Minute"
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: "Too Many Requests, Please Try Again After 1 Minute",
 });
 
 module.exports = limiter;
