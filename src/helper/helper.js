35aa4f2306d1c1fb6bc3133984c55a39b58f5070
JanviMangukiya <janvimangukiya1708@gmail.com>
2025-11-21 11:57:47 +0530

    added google pub/sub for reminder email

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,33 +1,33 @@
 const nodemailer = require("nodemailer");
 
 const successHandle = (req, res, message, statusCode, data = {}) => {
-    return res.status(statusCode).json({message, data});
-}
+  return res.status(statusCode).json({ message, data });
+};
 
 const errorHandle = (req, res, message, statusCode, data = {}) => {
-    return res.status(statusCode).json({message, data});
-}
+  return res.status(statusCode).json({ message, data });
+};
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GOOGLE_USER,
-        pass: process.env.GOOGLE_APP_PASSWORD,
-    },
+  service: "gmail",
+  auth: {
+    user: process.env.GOOGLE_USER,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
 });
 
-const sendEmail = async(to, subject, html) => {
-    try {
-        await transporter.sendMail({
-            from: `${process.env.GOOGLE_USER}`,
-            to,
-            subject,
-            html,
-        });
-        console.log("Email Sent Successfully", to);
-    } catch (error) {
-        console.error("Failed to Send Email", error.message);
-    }
+const sendEmail = async (to, subject, html) => {
+  try {
+    await transporter.sendMail({
+      from: `${process.env.GOOGLE_USER}`,
+      to,
+      subject,
+      html,
+    });
+    console.log("Email Sent Successfully", to);
+  } catch (error) {
+    console.error("Failed to Send Email", error.message);
+  }
 };
 
 module.exports = { successHandle, errorHandle, sendEmail };
--- src/utils/taskReminder.js
+++ src/utils/taskReminder.js
@@ -1,41 +1,46 @@
-const cron = require('node-cron');
-const Task = require('../models/taskModel');
-const { sendEmail } = require('../helper/helper');
+const cron = require("node-cron");
+const Task = require("../models/taskModel");
+const { publishMessage } = require("./googlePubSub");
+const batchSize = parseInt(process.env.BATCH_SIZE) || 100;
 
 const checkAndSendReminders = async () => {
-    try {
-        const today = new Date();
-        const tomorrow = new Date(today);
-        tomorrow.setDate(tomorrow.getDate() + 1);
+  try {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
 
-        const startOfTomorrow = new Date(tomorrow.toDateString()); 
-        const endOfTomorrow = new Date(tomorrow.toDateString());
-        endOfTomorrow.setDate(endOfTomorrow.getDate() + 1); 
-    
-        const tasks = await Task.find({
-            dueDate: { $gte: startOfTomorrow, $lt: endOfTomorrow },
-            isDeleted: false
-        }).populate('userId', 'email');
+    const startOfTomorrow = new Date(tomorrow.toDateString());
+    const endOfTomorrow = new Date(tomorrow.toDateString());
+    endOfTomorrow.setDate(endOfTomorrow.getDate() + 1);
 
-        for (const task of tasks) {
-            if (task?.userId?.email) {
-                await sendEmail(
-                    task.userId.email,
-                    'Task Due Tomorrow',
-                    `<h1>Task Reminder</h1>
-                     <p>Tomorrow is your Due Date for the Task: ${task.taskName}</p>
-                     <p>Due Date: ${task.dueDate}</p>`
-                );
-                console.log("Reminder Sent for Task");
-            }
-        }
-    } catch (error) {
-        console.error("Error in Task Reminder", error.message);
+    const tasks = await Task.find({
+      dueDate: { $gte: startOfTomorrow, $lt: endOfTomorrow },
+      isDeleted: false,
+    }).populate("userId", "email");
+
+    const totalTasks = tasks.length;
+    const chunkSize = Math.ceil(totalTasks / batchSize);
+    for (let i = 0; i < chunkSize; i++) {
+      const startIndex = i * batchSize;
+      const endIndex = startIndex + batchSize;
+      const batch = tasks.slice(startIndex, endIndex);
+
+      const data = batch
+        .filter((task) => task?.userId?.email)
+        .map((task) => ({
+          email: task?.userId?.email,
+          taskName: task?.taskName,
+          dueDate: task?.dueDate,
+        }));
+      await publishMessage("UserCreation", data);
     }
+  } catch (error) {
+    console.error("Error in Task Reminder", error.message);
+  }
 };
 
 const startTaskReminderJob = () => {
-    cron.schedule('0 9 * * *', checkAndSendReminders); 
+  cron.schedule("0 9 * * *", checkAndSendReminders);
 };
 
 module.exports = { startTaskReminderJob };
