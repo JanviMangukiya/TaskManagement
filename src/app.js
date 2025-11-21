4782cb297513d452a1efadb2590dd508744f5a25
JanviMangukiya <janvimangukiya1708@gmail.com>
2025-11-21 17:16:54 +0530

    added file structure

--- src/app.js
+++ src/app.js
@@ -3,29 +3,31 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const express = require("express");
 const rateLimit = require("./middleware/rateLimiter");
-const app = express();
 
-app.use(rateLimit);
-app.use(express.json());
 require("./db");
 
 const { startTaskReminderJob } = require("./utils/taskReminder");
-startTaskReminderJob();
-
-const { listenMessage } = require("./utils/googlePubSub");
-listenMessage("UserCreation-sub");
+const { listenMessage } = require("./services/googlePubSub");
 
 const adminRouter = require("./routes/adminRoutes");
 const userRouter = require("./routes/userRoutes");
 const taskRouter = require("./routes/taskRoutes");
 const taskStatusRouter = require("./routes/taskStatusRoutes");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(rateLimit);
+app.use(express.json());
+
+startTaskReminderJob();
+listenMessage("UserCreation-sub");
+
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 app.use("/task", taskRouter);
 app.use("/status", taskStatusRouter);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
